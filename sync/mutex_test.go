c885544456b283868fa6acaab20e7b33b18c26ad
Daniel Carbone <dcarbone@ena.com>
2018-03-22 21:08:56 -0500

    updating test a bit.

--- sync/mutex_test.go
+++ sync/mutex_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestMutex(t *testing.T) {
-	t.Parallel()
 	m := sync.Mutex{}
 	hit := ""
 
@@ -43,43 +42,22 @@ func TestMutex(t *testing.T) {
 }
 
 func TestMutex_Stale(t *testing.T) {
-	t.Parallel()
 	m := sync.Mutex{}
 
 	wg := new(gosync.WaitGroup)
 	wg.Add(3)
-	t.Run("routine-1", func(t *testing.T) {
-		for i := 0; i < 5; i++ {
-			m.Lock()
-			s := rand.Intn(5)
-			t.Logf("routine-1 acquired lock, sleeping for %d seconds", s)
-			time.Sleep(time.Duration(s) * time.Second)
-			m.Unlock()
-		}
-		wg.Done()
-	})
-
-	t.Run("routine-2", func(t *testing.T) {
-		for i := 0; i < 5; i++ {
-			m.Lock()
-			s := rand.Intn(5)
-			t.Logf("routine-2 acquired lock, sleeping for %d seconds", s)
-			time.Sleep(time.Duration(s) * time.Second)
-			m.Unlock()
-		}
-		wg.Done()
-	})
-
-	t.Run("routine-3", func(t *testing.T) {
-		for i := 0; i < 5; i++ {
-			m.Lock()
-			s := rand.Intn(5)
-			t.Logf("routine-3 acquired lock, sleeping for %d seconds", s)
-			time.Sleep(time.Duration(s) * time.Second)
-			m.Unlock()
-		}
-		wg.Done()
-	})
+	for i := 0; i < 3; i++ {
+		go func(t *testing.T, ii int) {
+			for i := 0; i < 5; i++ {
+				m.Lock()
+				s := rand.Intn(5)
+				t.Logf("routine-%d acquired lock, sleeping for %d seconds", ii, s)
+				time.Sleep(time.Duration(s) * time.Second)
+				m.Unlock()
+			}
+			wg.Done()
+		}(t, i)
+	}
 
 	wg.Wait()
 }
